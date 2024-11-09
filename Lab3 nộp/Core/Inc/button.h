242dd2e64a24b4464a0860b71916f5cf3822e5fa
Cheems123-stack <khoi.nguyen2252379@hcmut.edu.vn>
2024-11-09 10:18:02 +0700

    Add files via upload

---
+++ Lab3 nộp/Core/Inc/button.h
@@ -0,0 +1,15 @@
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+#include "main.h"
+
+#define NORMAL_STATE GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+#define NUMBER_BUTTON	3
+
+extern int button_flag[];
+extern int button_pressed_1s_flag[];
+
+void getKeyInput();
+void KeyProcess(int index);
+
+#endif
---
+++ Lab3 nộp/Core/Inc/green_mode.h
@@ -0,0 +1,9 @@
+#ifndef INC_GREEN_MODE_H_
+#define INC_GREEN_MODE_H_
+
+#include "main.h"
+
+void greenMode();
+void setTimeGreen();
+
+#endif
