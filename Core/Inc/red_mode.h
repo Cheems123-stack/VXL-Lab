4a8a71e40cda6e5dcf092b24c5e079e68b5c196e
Cheems123-stack <khoi.nguyen2252379@hcmut.edu.vn>
2024-11-09 10:13:36 +0700

    Add files via upload

---
+++ Core/Inc/red_mode.h
@@ -0,0 +1,17 @@
+/*
+ * input_reading.h
+ *
+ *  Created on: Nov 16, 2022
+ *      Author: banhb
+ */
+
+#ifndef INC_RED_MODE_H_
+#define INC_RED_MODE_H_
+
+#include "main.h"
+
+void redMode();
+void setTimeRed();
+
+
+#endif  /* INC_RED_MODE_H_ */
---
+++ Core/Inc/yellow_mode.h
@@ -0,0 +1,9 @@
+#ifndef INC_YELLOW_MODE_H_
+#define INC_YELLOW_MODE_H_
+
+#include "main.h"
+
+void yellowMode();
+void setTimeYellow();
+
+#endif
